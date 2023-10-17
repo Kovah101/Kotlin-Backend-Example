75d85cd99de64c88605c22ff430d9e1b486b2c7b
Elliot Thompson <elliotthompson101@gmail.com>
2023-10-17 14:03:56 -0700

    Initial Spring Boot setup

---
+++ src/test/kotlin/com/kovah101/backenddemo/BackendDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kovah101.backenddemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
